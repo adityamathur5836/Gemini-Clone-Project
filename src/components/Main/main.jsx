6e74b18e76c2934169de164fb3a1f33c04d112d4
Aditya Mathur <aditya.2024@Adityas-MacBook-Pro-7.local>
2025-02-12 20:27:46 +0530

    Navbar Created

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,1 +1,16 @@
-git 
+import React from 'react'
+import './main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt=''/>
+        </div>
+    </div>
+  )
+}
+
+export default Main

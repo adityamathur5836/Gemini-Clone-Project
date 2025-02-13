8d6b562a11289194b8af05bfbf85dfa00f208374
Aditya Mathur <aditya.2024@Adityas-MacBook-Pro-7.local>
2025-02-13 16:53:06 +0530

    Main Platform Created

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/context'
 
 const Main = () => {
+
+    const {onSent,recentPrompt,showResult,loading,resultData,input ,setInput} = useContext(Context)
+
   return (
     <div className='main'>
         <div className="nav">
@@ -10,6 +14,9 @@ const Main = () => {
             <img src={assets.user_icon} alt=''/>
         </div>
         <div className="main-container">
+
+            {!showResult
+            ?<>
             <div className="greet">
                 <p><span>Hello, User.</span></p>
                 <p>How can I help you today?</p>
@@ -32,13 +39,36 @@ const Main = () => {
                     <img src={assets.code_icon} alt=''/>
                 </div>
             </div>
+            </>
+            :<div className='result'>
+                <div className="result-title">
+                    <img src={assets.user_icon} alt=''/>
+                    <p>{recentPrompt}</p>
+                </div>
+                <div className="result-data">
+                    <img src={assets.gemini_icon} alt=''/>
+                    {loading ? 
+                    <div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div> 
+                    :
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                    }
+                    
+                </div>
+            </div>
+        }
+
+            
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type='text' placeholder='Ask Gemini...'/>
+                    <input onChange={(e) => setInput(e.target.value)} value={input} type='text' placeholder='Ask Gemini...'/>
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                     </div>
                 </div>
                 <p className="bottom-info">
